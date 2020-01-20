165114cf84fa90469d8b18028c0003bd25cf4fcc
shhaozhu <simon.zhu.chn@hotmail.com>
2020-01-20 21:55:53 +0800

    add netty

---
+++ zsh-springboot-netty/src/main/java/zsh/springboot/netty/NettyServerChannelInitializer.java
@@ -0,0 +1,19 @@
+package zsh.springboot.netty;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.util.CharsetUtil;
+import zsh.springboot.netty.handler.NettyServerHandler;
+import zsh.springboot.netty.handler.NettyServerHttpHandler;
+
+public class NettyServerChannelInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel socketChannel) throws Exception {
+        //添加编解码
+        socketChannel.pipeline().addLast("decoder", new StringDecoder(CharsetUtil.UTF_8));
+        socketChannel.pipeline().addLast("encoder", new StringEncoder(CharsetUtil.UTF_8));
+        socketChannel.pipeline().addLast(new NettyServerHttpHandler());
+    }
+}
---
+++ zsh-springboot-netty/src/main/java/zsh/springboot/netty/handler/NettyServerHttpHandler.java
@@ -0,0 +1,61 @@
+package zsh.springboot.netty.handler;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.*;
+import lombok.extern.slf4j.Slf4j;
+
+import static io.netty.handler.codec.http.HttpHeaders.Names.CONNECTION;
+import static io.netty.handler.codec.http.HttpHeaders.Names.CONTENT_LENGTH;
+import static io.netty.handler.codec.http.HttpHeaders.Names.CONTENT_TYPE;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
+
+@Slf4j
+public class NettyServerHttpHandler extends ChannelInboundHandlerAdapter {
+
+    private HttpRequest request;
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg)
+            throws Exception {
+        if (msg instanceof HttpRequest) {
+            request = (HttpRequest) msg;
+
+            String uri = request.getUri();
+            System.out.println("Uri:" + uri);
+        }
+        if (msg instanceof HttpContent) {
+            HttpContent content = (HttpContent) msg;
+            ByteBuf buf = content.content();
+            System.out.println(buf.toString(io.netty.util.CharsetUtil.UTF_8));
+            buf.release();
+
+            String res = "I am OK";
+            FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1,
+                    OK, Unpooled.wrappedBuffer(res.getBytes("UTF-8")));
+            response.headers().set(CONTENT_TYPE, "text/plain");
+            response.headers().set(CONTENT_LENGTH,
+                    response.content().readableBytes());
+            if (HttpHeaders.isKeepAlive(request)) {
+                response.headers().set(CONNECTION, HttpHeaders.Values.KEEP_ALIVE);
+            }
+            ctx.write(response);
+            ctx.flush();
+        }
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        log.error(cause.getMessage());
+        ctx.close();
+    }
+
+}
