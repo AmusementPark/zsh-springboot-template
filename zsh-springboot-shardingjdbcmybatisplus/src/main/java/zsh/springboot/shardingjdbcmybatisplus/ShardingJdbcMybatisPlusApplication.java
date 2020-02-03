12bc89630b76c4903974f40b2a53ef1db63cc2fb
shhaozhu <simon.zhu.chn@hotmail.com>
2020-02-04 02:18:43 +0800

    add mybatisplus shardingjdbc template

---
+++ zsh-springboot-shardingjdbcmybatisplus/src/main/java/zsh/springboot/shardingjdbcmybatisplus/ShardingJdbcMybatisPlusApplication.java
@@ -0,0 +1,13 @@
+package zsh.springboot.shardingjdbcmybatisplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingJdbcMybatisPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingJdbcMybatisPlusApplication.class, args);
+    }
+
+}
---
+++ zsh-springboot-shardingjdbcmybatisplus/src/test/java/zsh/springboot/shardingjdbcmybatisplus/ShardingjdbcmybatisplusApplicationTests.java
@@ -0,0 +1,13 @@
+package zsh.springboot.shardingjdbcmybatisplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShardingjdbcmybatisplusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
