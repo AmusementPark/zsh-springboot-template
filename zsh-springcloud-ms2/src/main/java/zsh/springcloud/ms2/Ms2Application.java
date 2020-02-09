c61710bec286d90f7909fd466c2cbee4050ddc28
Shenghao.Zhu <Shenghao.Zhu@Geely.com>
2020-02-09 13:59:50 +0800

    add skywalking

--- zsh-springcloud-ms2/src/main/java/zsh/springcloud/ms2/Ms2Application.java
+++ zsh-springcloud-ms2/src/main/java/zsh/springcloud/ms2/Ms2Application.java
@@ -2,7 +2,11 @@ package zsh.springcloud.ms2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("api")
 @SpringBootApplication
 public class Ms2Application {
 
@@ -10,4 +14,8 @@ public class Ms2Application {
         SpringApplication.run(Ms2Application.class, args);
     }
 
+    @RequestMapping("get")
+    public String alohaFromMicroService2() {
+        return "Aloha From MS2!";
+    }
 }
